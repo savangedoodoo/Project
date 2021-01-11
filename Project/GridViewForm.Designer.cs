6c8c54ce9052e3d260da336158e6d7078e236f76
Lặc <expertdicer@gmail.com>
2021-01-11 10:29:41 +0700

    Add GridViewForm
    Ôn cấu trúc dữ liệu đây

---
+++ Project/GridViewForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Project
+{
+    partial class GridViewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.aBookBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.aBookBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aBookBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aBookBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.DataSource = this.aBookBindingSource1;
+            this.dataGridView1.Location = new System.Drawing.Point(93, 159);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(359, 214);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // aBookBindingSource
+            // 
+            this.aBookBindingSource.DataSource = typeof(Project.ABook);
+            // 
+            // aBookBindingSource1
+            // 
+            this.aBookBindingSource1.DataSource = typeof(Project.ABook);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(505, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Sách đang mượn";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(505, 292);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 51);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Mượn sách";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(505, 79);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 40);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Tìm";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 79);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(359, 40);
+            this.textBox1.TabIndex = 4;
+            // 
+            // GridViewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 385);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "GridViewForm";
+            this.Text = "GridViewForm";
+            this.Load += new System.EventHandler(this.GridViewForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aBookBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aBookBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource aBookBindingSource;
+        private System.Windows.Forms.BindingSource aBookBindingSource1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
